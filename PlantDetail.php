7d93a7cda168fd37c363157a809b98b89cd36e16
jongwon <90269036+jonnygim@users.noreply.github.com>
2022-02-23 10:00:48 +0900

    [Refactor] PlantDetail

--- PlantDetail.php
+++ PlantDetail.php
@@ -3,7 +3,7 @@ include "connect.php";
 
 $ch = curl_init();
 $url = 'http://api.nongsaro.go.kr/service/garden/gardenDtl';
-$queryParams = '?'.urlencode('apiKey').'='.'20210908LGXOY6G03MU6JAYF22EEQ';
+$queryParams = '?'.urlencode('apiKey').'='.'??';
 $queryParams .= '&'.urlencode('cntntsNo'). '='.urlencode('12938');
 
 curl_setopt($ch, CURLOPT_URL, $url.$queryParams);
@@ -13,45 +13,14 @@ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 $response = curl_exec($ch);
 curl_close($ch);
 
-$idx=1;
 $xml = simplexml_load_string($response);
 $items = $xml->body->item;
-var_dump($items);
 
 foreach ($items as $item) {
 	echo($item->fmlNm)."<br>";
-    echo($item->orgplceInfo)."<br>";
-   
-
-    echo($item->clCodeNm)."<br>"."<br>";
-    }
-    // distbNm
-    // orgplceInfo 원산지
-    // adviseInfo 조언정보
-    // toxctyInfo 독성정보
-    // fmlNm 과명
-    // growthHgInfo
-    // managelevelCodeNm
-    // grwhTpCodeNm
-    // winterLwetTpCodeNm
-    // hdCodeNm
-    // frtlzrInfo
-    // soilInfo
-    // watercycleSprngCodeNm
-    // watercycleSummerCodeNm
-    // watercycleAutumnCodeNm
-    // watercycleWinterCodeNm
-    // dlthtsManageInfo
-    // speclmanageInfo
-    // fncltyInfo
-    // managedemanddoCodeNm
-    // indoorpsncpacompositionCodeNm
-    // ignSeasonCodeNm
-    // prpgtmthCodeNm
-    // lighttdemanddoCodeNm
-    // postngplaceCodeNm
-    // dlthtsCodeNm
-
+	echo($item->orgplceInfo)."<br>";
+	echo($item->clCodeNm)."<br>"."<br>";
+}
 
 echo "<pre>";
 echo $arr;
