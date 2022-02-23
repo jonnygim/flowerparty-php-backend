b40cc73234bb6473f0473e8c044a160f1a6f2d45
jongwon <90269036+jonnygim@users.noreply.github.com>
2022-02-23 09:32:00 +0900

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,11 +1,10 @@
 <?php
-//$con = mysqli_connect('localhost', 'flowerparty', 'fp!550149', 'flowerparty');
-$con = mysqli_connect('localhost', 'ci2021flower', '2021flower', 'ci2021flower');
+$con = mysqli_connect('localhost', '??', '??', '??');
 mysqli_query($con, 'SET NAMES utf8');
 
-// if(!$con) {
-//     echo "mysql 접속 에러";
-//     echo myspli_connect_error();
-//     exit;
-// }
+if(!$con) {
+    echo "mysql 접속 에러";
+    echo myspli_connect_error();
+    exit;
+}
 ?>
