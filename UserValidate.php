53681e9a14688f549243c77ba394450a946cc2a8
jongwon <90269036+jonnygim@users.noreply.github.com>
2022-02-23 09:50:51 +0900

    Update UserValidate.php

--- UserValidate.php
+++ UserValidate.php
@@ -1,6 +1,5 @@
 <?php
 include "connect.php";
-//$con = mysqli_connect('localhost', 'flowerparty', 'fp!550149', 'flowerparty');    mysqli_query($con, 'SET NAMES utf8');
 
 $userID = $_POST["userID"];
 $userPassword = $_POST["userPassword"];
@@ -10,20 +9,16 @@ $statement = mysqli_prepare($con, "SELECT `userID` FROM `user` WHERE `userID` = 
 mysqli_stmt_bind_param($statement, "s", $userID);
 mysqli_stmt_execute($statement);
 
-
 mysqli_stmt_store_result($statement);
 mysqli_stmt_bind_result($statement, $userID);
 
 $response = array();
 $response["success"] = true;
 
-
-
 while(mysqli_stmt_fetch($statement)){
     $response["success"] = false;
     $response["userID"] = $userID;
 }
-
 
 echo json_encode($response);
 ?>
