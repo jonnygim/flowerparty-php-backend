c4132d91961cc3e371974b736b96f3fc9c158b1d
jongwon <90269036+jonnygim@users.noreply.github.com>
2022-02-23 09:59:05 +0900

    [Refactor] Plant

--- Plant.php
+++ Plant.php
@@ -3,7 +3,7 @@ mysqli_query($con, 'SET NAMES utf8');
 
 $ch = curl_init();
 $url = 'http://api.nongsaro.go.kr/service/garden/gardenList';
-$queryParams = '?'.urlencode('apiKey').'='.'20210908LGXOY6G03MU6JAYF22EEQ';
+$queryParams = '?'.urlencode('apiKey').'='.'??';
 $queryParams .= '&'.urlencode('numOfRows'). '='.urlencode('217');
 
 curl_setopt($ch, CURLOPT_URL, $url.$queryParams);
@@ -13,18 +13,15 @@ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 $response = curl_exec($ch);
 curl_close($ch);
 
-$idx=1;
 $xml = simplexml_load_string($response);
 $sql = "INSERT INTO `plant`(cntntsNo, cntntsSj) VALUES('$cntntsNo', '$cntntsSj')";
 
 print_r(sizeof($xml->body->items->item));
 
-for ($k = 0; $k <217; $k++) {
-
-}
 $arr = array(array());
 //$arr = $xml->body->items->item[1]->cntntsNo
 
+// xml 파일에 '' 가 있어서 직접 수정함
 for ($i=0; $i<sizeof($xml->body->items->item); $i++) {
     for ($j=0; $j<2; $j++) {
         if ($j == 0) {
@@ -35,19 +32,13 @@ for ($i=0; $i<sizeof($xml->body->items->item); $i++) {
             //echo $arr[$i][$j];
         }
     }
-    
-    $idx = $i+1;
     $sub_sql_array[] = " ( '{$arr[$i][0]}', '{$arr[$i][1]}' ) "; 
-    
-  
-    //echo "<br>\n";
-    
-
 }
+
 $sql = "INSERT INTO `plant`(cntntsNo, cntntsSj) VALUES";
 $sub_sql = implode(', ', $sub_sql_array); 
 $sql .= $sub_sql; 
 echo $sql;
 mysqli_query($con, $sql);
-// xml 파일에 '' 가 있어서 직접 수정함
+
 exit;
